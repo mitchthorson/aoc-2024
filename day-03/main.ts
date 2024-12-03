355cb54e457ee406600e2a2e9f594b3cbfd34e14
mitchthorson <mitchell@mitchellthorson.com>
2024-12-03 18:24:54 -0500

    day-03 part 2

--- day-03/main.ts
+++ day-03/main.ts
@@ -2,7 +2,7 @@ export function solve(input: string): number {
   const pattern = /mul\((\d{1,3}),(\d{1,3})\)/g;
   const matches = Array.from(input.matchAll(pattern));
   const result = matches
-    .map(([_, a, b, ...rest]) => {
+    .map(([_, a, b]) => {
       return Number(a) * Number(b);
     })
     .reduce((acc, val) => acc + val, 0);
@@ -10,11 +10,35 @@ export function solve(input: string): number {
 }
 
 export function solveTwo(input: string): number {
-  return 1;
+  const pattern = /mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don\'t\(\))/g;
+  const matches = Array.from(input.matchAll(pattern));
+  const instructions = matches.map(([instruction, a, b]) => {
+    if (instruction.includes("mul")) {
+      return Number(a) * Number(b);
+    }
+    return instruction;
+  });
+  let result = 0;
+  let enabled = true;
+  instructions.forEach((instruction) => {
+    switch (instruction) {
+      case "do()":
+        enabled = true;
+        break;
+      case "don't()":
+        enabled = false;
+        break;
+      default:
+        if (enabled && typeof instruction === "number") {
+          result += instruction;
+        }
+    }
+  });
+  return result;
 }
 
 if (import.meta.main) {
   const input = Deno.readTextFileSync("input");
   console.log("Problem one:", solve(input));
-  // console.log("Problem two:", solveTwo(input));
+  console.log("Problem two:", solveTwo(input));
 }
--- day-03/main_test.ts
+++ day-03/main_test.ts
@@ -11,12 +11,13 @@ Deno.test({
   },
 });
 
-// Deno.test({
-//   name: "two",
-//   fn() {
-//     const input = Deno.readTextFileSync("input_test");
-//     const result = solveTwo(input);
-//     const answer = 4;
-//     assertEquals(result, answer);
-//   },
-// });
+Deno.test({
+  name: "two",
+  fn() {
+    const input =
+      "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+    const result = solveTwo(input);
+    const answer = 48;
+    assertEquals(result, answer);
+  },
+});
